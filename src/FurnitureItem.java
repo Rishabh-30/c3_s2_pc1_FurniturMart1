1604f483b300a9a3716a1a1d8b8cc5a55aecf820
Rishabh Pathak <rishabh.pathak30@hotmail.com>
2022-08-11 13:25:46 +0530

    Solution

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class FurnitureItem {
+public class FurnitureItem {
+    int furnitureCode;
+    String furnitureType;
+    String gradeOfFurniture;
+    String color;
+    String furnitureUsage;
+    int price;
+
+    FurnitureItem(){
+        furnitureCode = 0;
+        furnitureType = "";
+        gradeOfFurniture= "";
+        color = "";
+        furnitureUsage = "";
+        price = 0;
+
+
+
+    }
+    double discount(double price){
+        double discount = (price -(price * 5/100));
+        return discount;
+    }
+
 }
--- src/FurnitureItemImpl.java
+++ src/FurnitureItemImpl.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class FurnitureItemImpl {
+import java.util.Scanner;
+
+public class FurnitureItemImpl {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter furniture code");
+        int Code = scanner.nextInt();
+        System.out.println("Enter furnitureType");
+        scanner.nextLine();
+        String type = scanner.nextLine();
+        System.out.println("Enter Grade");
+        String grade = scanner.nextLine();
+        System.out.println("Enter color");
+        String color = scanner.nextLine();
+        System.out.println("Enter furniture usage");
+        String usage = scanner.nextLine();
+        System.out.println("Enter price");
+        int price = scanner.nextInt();
+
+        FurnitureItem furItem = new FurnitureItem();
+        System.out.println("furItem.discount() = " + furItem.discount(10000));
+
+
+
+
+    }
 }
